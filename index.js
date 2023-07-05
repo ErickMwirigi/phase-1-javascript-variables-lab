58f77fa93ceb8c003283e50f369c5f88813a2f43
ErickMwirigi <erickmbae21@gmail.com>
2023-07-05 11:04:52 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
